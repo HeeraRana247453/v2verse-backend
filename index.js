70b4f0de771e9f3c7c889ec47ab461ed740aa700
Heera Rana <heerarana526@gmail.com>
2025-01-24 23:20:37 +0530

    change in vercel file

--- index.js
+++ index.js
@@ -30,16 +30,16 @@ const message = require("./controller/message.js");
 const withdraw = require("./controller/withdraw/withdraw.js");
 
 // ROUTES
-app.use("/api/v2/user",user);
-app.use("/api/v2/conversation", conversation);
-app.use("/api/v2/message", message);
-app.use("/api/v2/order", order);
-app.use("/api/v2/shop",shop);
-app.use("/api/v2/product",product);
-app.use("/api/v2/event",event);
-app.use("/api/v2/coupon",coupon);
-app.use("/api/v2/payment", payment);
-app.use("/api/v2/withdraw", withdraw);
+app.use("/user",user);
+app.use("/conversation", conversation);
+app.use("/message", message);
+app.use("/order", order);
+app.use("/shop",shop);
+app.use("/product",product);
+app.use("/event",event);
+app.use("/coupon",coupon);
+app.use("/payment", payment);
+app.use("/withdraw", withdraw);
 
 // it's for ErrorHandling
 app.use(ErrorHandler);
