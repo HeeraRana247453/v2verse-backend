bc3c8fe4062cc3385525518ff74dec7c47e5e191
Heera Rana <heerarana526@gmail.com>
2025-01-24 23:07:45 +0530

    change file name to index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const ErrorHandler = require("./middleware/error");
+const ErrorHandler = require("./middleware/error.js");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -18,11 +18,11 @@ app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 // app.use("/", express.static("uploads"));//to make the 'uploads' folder accessible globally
 
 
-const user = require("./controller/user/user");
-const shop = require("./controller/shop/shop");
-const product = require("./controller/product/product");
-const event = require("./controller/events/event");
-const coupon = require("./controller/couponCode/couponCode");
+const user = require("./controller/user/user.js");
+const shop = require("./controller/shop/shop.js");
+const product = require("./controller/product/product.js");
+const event = require("./controller/events/event.js");
+const coupon = require("./controller/couponCode/couponCode.js");
 const payment = require("./controller/payment/payment.js");
 const order = require("./controller/order/order.js");
 const conversation = require("./controller/conversation/conversation.js");
