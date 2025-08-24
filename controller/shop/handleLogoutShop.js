f4b8f2fe48b327c993394e1028da90c8ac615ec5
Heera Rana <heerarana79352@gmail.com>
2025-08-24 23:28:08 +0530

    rollback: to previous state

--- controller/shop/handleLogoutShop.js
+++ controller/shop/handleLogoutShop.js
@@ -3,14 +3,12 @@ const ErrorHandler = require("../../utils/ErrorHandler");
 
 const handleLogoutShop = catchAsyncErrors(async (req, res, next) => {
     try {
-      res.clearCookie("seller_token", null, {
-        expires: new Date(0),
+      res.cookie("seller_token", null, {
+        expires: new Date(Date.now()),
         httpOnly: true,
         sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",// Use 'None' for production, 'Lax' for development,
         secure: process.env.NODE_ENV === "production",
-        path: "/", 
       });
-      res.setHeader("Cache-Control", "no-store"); // avoid cached GETs
       res.status(200).json({
         success: true,
         message: "Log out successful!",
